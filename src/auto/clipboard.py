e5a6ebe159fbef3cb5ecf35498de4b386a27a19f
Jin Bin <jinbin@weidian.com>
2020-04-15 18:36:04 +0800

    commit clipboard.py

--- src/auto/clipboard.py
+++ src/auto/clipboard.py
@@ -3,7 +3,9 @@
 
 from AppKit import NSPasteboard, NSPasteboardTypePNG, NSPasteboardTypeTIFF, NSPasteboardTypePDF, NSPasteboardTypeString
 from objc._pycoder import NSArray
+from PIL import Image
 
+from src.utils.utils import utils
 
 def get_paste_img_file():
     """
@@ -11,8 +13,6 @@ def get_paste_img_file():
     """
     pb = NSPasteboard.generalPasteboard()  # 获取当前系统剪切板数据
     data_type = pb.types()  # 获取剪切板数据的格式类型
-    # print(pb)
-    # print(data_type)
 
     # 根据剪切板数据类型进行处理
     if NSPasteboardTypePNG in data_type:          # PNG处理
@@ -29,16 +29,15 @@ def get_paste_img_file():
         print("NSPasteboardTypePDF")
         pass
 
-def writeToFile(type):
-    print("type: " + type)
-    pb = NSPasteboard.generalPasteboard()
-    data = pb.dataForType_(type)
-    filename = type
-    filepath = '/tmp/%s' % filename
+# 将剪切板数据写入到指定文件
+def writeToFile(data):
+    filepath = '/tmp/%s' % "test.file"
     ret = data.writeToFile_atomically_(filepath, False)
     if ret:
         return filepath
 
+# 设置剪切板的内容
+# TODO 目前只支持String，图片等还需要另外支持
 def setClip (text):
     pb = NSPasteboard.generalPasteboard()
     pb.clearContents()
@@ -46,8 +45,26 @@ def setClip (text):
     a = NSArray.arrayWithObject_(text)
     return pb.writeObjects_(a)
 
+# 将bytes数据还原为图片
+def image_from_bytes(raw):
+    from_img = Image.frombytes(img.mode, img.size, raw)
+    return from_img
+
 if __name__ == '__main__':
     text = "激战"
     setClip(text)
     # bytes字节符，打印以b开头。
+    # encode() will result in a sequence of bytes. decode() decodes a stream of bytes to a string object
+    # bytes([source[, encoding[, errors]]]): source to initialize the array of bytes. if source is a string, the encoding of the string.
     print(bytes(get_paste_img_file()).decode("utf-8"))
+
+    data = get_paste_img_file()
+    writeToFile(data)
+
+    # 将bytes数据还原为图片
+    # TODO 如何将剪切板获取的数据转成合适的bytes
+    img = Image.open(utils.getDataDir("/images/gushi.jpeg"))
+    raw = img.tobytes()
+
+    img_out = image_from_bytes(raw)
+    img_out.show()
